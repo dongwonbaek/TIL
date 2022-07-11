c936139ce0851ca93fedbb765d275140ad9125a4
dongwonbaek <bmrio100@gmail.com>
2022-07-11 16:31:10 +0900

    python_codeup

---
+++ python_codeup/codeup/codeup_6025.py
@@ -0,0 +1,12 @@
+a,b = input().split()
+a = int(a)
+b = int(b)
+c = int(a)+int(b)
+print(c)
+
+
+# a = input()
+# b = input()
+# print(int(a) + int(b))
+# input() 은 모두 string으로 저장된다.
+# 그러므로 숫자로 활용하기 위해서는 항상 int로 변환해야함.
