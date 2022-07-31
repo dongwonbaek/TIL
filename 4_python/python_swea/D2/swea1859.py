3073cda329be90b978fa6aba650059c381c5c211
dongwonbaek <bmrio100@gmail.com>
2022-07-31 18:01:13 +0900

    swea 문제풀이

---
+++ 4_python/python_swea/D2/swea1859.py
@@ -0,0 +1,18 @@
+T = int(input())
+for _ in range(1, 1 + T):
+    len_numbers = int(input())
+    number_list = list(map(int, input().split()))
+    sum_final = 0
+    sum_ = []
+
+    for b in range(len_numbers - 1):
+        if number_list[b] <= number_list[b + 1]:
+            sum_.append(number_list[b + 1] - number_list[b])
+            if b == len_numbers - 2:
+                for c in range(len(sum_)):
+                    sum_final += sum_[c] * (c + 1)
+        else:
+            for c in range(len(sum_)):
+                sum_final += sum_[c] * (c + 1)
+            sum_ = []
+    print(f'#{_} {sum_final}')
