ef0b851b6d907b2cf28794db719f863374f99f45
dongwonbaek <bmrio100@gmail.com>
2022-08-21 20:45:41 +0900

    swea 문제풀이

---
+++ 4_python/python_swea/D3/1213.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('1213.txt', encoding = 'UTF8')
+
+for _ in range(1, 11):
+    number = input()
+    word = input()
+    # word_list = str(input())
+    result = ''
+    cnt = 0
+    for a in input():
+        result += a
+        if word in result:
+            cnt += 1
+            result = ''
+    print(f'#{_} {cnt}')
