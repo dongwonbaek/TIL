8698259badbec5c7c03b4e3cf4d001f528eadebd
dongwonbaek <bmrio100@gmail.com>
2022-09-26 17:19:14 +0900

    django 실습

---
+++ 7_django/django_0926/checking_odd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CheckingOddConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'checking_odd'
---
+++ 7_django/django_0926/checking_odd/views.py
@@ -0,0 +1,107 @@
+import random
+from django.shortcuts import render
+
+# Create your views here.
+def checking_odd(request, number):
+    if number == 0:
+        text = "0"
+    else:
+        if number % 2 == 0:
+            text = "짝수"
+        else:
+            text = "홀수"
+    context = {
+        "number": number,
+        "text": text,
+    }
+    return render(request, "checking_odd.html", context)
+
+
+def calculate(request, number_a, number_b):
+
+    context = {
+        "number_a": number_a,
+        "number_b": number_b,
+        "plus_": number_a + number_b,
+        "minus_": number_a - number_b,
+        "division_": number_a // number_b,
+        "multiply_": number_a * number_b,
+    }
+    return render(request, "calculate.html", context)
+
+
+def animal(request):
+
+    return render(request, "animal.html")
+
+
+def result(request):
+
+    animal = [
+        "말",
+        "소",
+        "개",
+        "양",
+        "호랑이",
+        "고양이",
+        "쥐",
+        "닭",
+        "용",
+        "염소",
+        "돼지",
+        "뱀",
+        "모기",
+        "개미",
+        "매미",
+        "낙타",
+        "코끼리",
+        "기린",
+        "사자",
+        "다람쥐",
+        "사람",
+        "독수리",
+        "오리",
+        "백조",
+        "지렁이",
+        "공작",
+    ]
+    context = {"animal": random.choice(animal), "name": request.GET["name"]}
+    return render(request, "result.html", context)
+
+
+def lorem(request):
+    return render(request, "lorem.html")
+
+
+def ipsum(request):
+    random_name = [
+        "짜장면 ",
+        "탕수육 ",
+        "짬뽕 ",
+        "육개장 ",
+        "배고프다 ",
+        "놀고싶다 ",
+        "핸드폰 ",
+        "컴퓨터 ",
+        "키보드 ",
+        "마우스 ",
+        "이어폰 ",
+        "마이크 ",
+        "코카콜라 ",
+    ]
+    num_paragraph = request.GET["num_paragraphs"]
+    num_word = request.GET["num_words"]
+    result2 = []
+    for b in range(int(num_paragraph)):
+        result1 = ""
+        for a in range(int(num_word)):
+            result1 += random.choice(random_name)
+        result2.append(result1)
+    context = {
+        "result": result2,
+    }
+    return render(request, "ipsum.html", context)
+
+
+def root(request):
+    return render(request, "root.html")
